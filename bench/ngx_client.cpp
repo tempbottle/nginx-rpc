01dfda09122c019e0e9750b5ed0e198bdb44bdbf
tanguofu <tanguofu@gmail.com>
2015-02-12 17:16:03 +0800

    test rpc client

--- bench/ngx_client.cpp
+++ bench/ngx_client.cpp
@@ -1,5 +1,21 @@
 #include "ngx_rpc_module/ngx_rpc_client_api.h"
 
+//add a event fd to poll
+
+//push a callback
+
+//ngx_cycle
+ngx_http_request_t * new_request(ngx_cycle_t *cycle)
+{
+      ngx_http_request_t * r = ngx_palloc(cycle->pool, sizeof(ngx_http_request_t));
+
+      memset(r,0, sizeof(ngx_http_request_t));
+
+      r->connection = ngx_get_connection(s, ev->log);
+
+
+
+}
 
 
 
@@ -7,6 +23,7 @@
 int main(int argc, char* argv[])
 {
     printf("hehe");
+
 
     ngx_main(argc, argv);
 
