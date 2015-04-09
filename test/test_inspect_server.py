3092c008c40597b08de2cb377cdf205954048519
tanguofu <tanguofu@gmail.com>
2015-04-09 22:00:38 +0800

    fix some bugs and cores

---
+++ test/test_inspect_server.py
@@ -0,0 +1,31 @@
+import sys
+import os
+
+#../thirdparty/protobuf/bin/protoc --python_out=.  --proto_path=../thirdparty/protobuf/include/google/protobuf  ../thirdparty/protobuf/include/google/protobuf/descriptor.proto
+
+
+sys.path.append(os.path.abspath(os.path.dirname(__file__)) + '/../thirdparty/protobuf/python')
+#import google
+#import google.protobuf 
+
+sys.path.append(os.path.abspath(os.path.dirname(__file__)) + '/../inspect_server/')
+from inspect_pb2 import *
+
+import urllib2
+
+if __name__ == '__main__':
+    
+    url = "http://10.25.66.77:8081/ngxrpc/inspect/application/interface"
+    
+    reqpb = Request()
+    reqpb.json = "hehe"
+    data = reqpb.SerializeToString()
+    
+    req = urllib2.Request(url)
+    req.add_header('Content-Type', 'application/json')
+
+    opener = urllib2.build_opener(urllib2.HTTPCookieProcessor()) 
+    response = opener.open(req, data) 
+    print response.read()
+    
+    
